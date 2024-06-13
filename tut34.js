436f82d92f43c99f698ce42aa14c85ecd74f6ac2
Vivek9988 <vivek19102004@gmail.com>
2024-06-13 11:01:22 +0530

    first commit

---
+++ tut34.js
@@ -0,0 +1,9 @@
+function vidhi( x , y){
+    console.log("done")
+    return(x+y)/2
+}
+console.log(vidhi(2,4))
+console.log(vidhi(2,4))
+console.log(vidhi(2,4))
+console.log(vidhi(2,4))
+console.log(vidhi(2,4))
